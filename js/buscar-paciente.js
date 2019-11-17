190695b10beaf8b168be225108500a43c62c5636
fabiovalinhos <fabiovalinhos@gmail.com>
2019-11-17 15:10:19 -0300

    Tratamento dos erros para no Ajax

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -8,13 +8,22 @@ botaoAdicionar.addEventListener("click", function(){
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta);
-        console.log(pacientes);
-
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+        
+        var erroAjax = document.querySelector("#erro-ajax");
+        if(xhr.status == 200){
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+            console.log(pacientes);
+    
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel");
+        }
         
     });
 
